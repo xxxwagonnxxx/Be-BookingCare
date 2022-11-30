e2a722fa460616a201c2ac88dbba141343cd8df5
xxxwagonnxxx <nguyentrungduc051100@gmail.com>
2022-11-30 14:24:53 +0700

    init

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,29 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
+    router.get('/crud', homeController.getCRUD);
+
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+
+    router.post('/put-crud', homeController.putCRUD);
+    router.get('/delete-crud', homeController.deleteCRUD);
+
+    router.post('/api/login', userController.handleLogin);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser);
+
+    router.get('/api/allcode', userController.getAllCode);
+
 
     return app.use("/", router);
 }
--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
+import connectDB from "./config/connectDB";
+import cors from 'cors';
+
 require('dotenv').config();
 
 let app = express();
+app.use(cors({ origin: true }));
+//config app    
 
-//config app
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969;
 //Port === undefined => port = 6969
